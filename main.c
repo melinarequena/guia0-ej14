00bc0976076873ae2cb7bf82077b5008c4fc3bf7
Melina <mrequena@fue.undef.edu.ar>
2023-04-19 19:21:14 -0300

    Ej 14, no me imprime el string

--- main.c
+++ main.c
@@ -1,3 +1,21 @@
 /*Defina la función copyString que recibe un puntero, que apuntará a una cadena de
 caracteres. Al ejecutarse la función, la misma deberá retornar una copia, en una
 nueva porción de memoria */
+
+#include<stdio.h>
+char copystring(char*, char);
+int main(){
+    char text[1000], *textptr, newtext, *newptr;
+    printf("Ingrese un texto:\n");
+    gets(text);
+    textptr = &text;
+    newtext=copystring(textptr, newtext);
+    newptr = &newtext;
+    printf("El texto regenerado en una nueva posicion de memora la cual es %p, es %s", newptr, newtext);
+    return 0;
+
+}
+char copystring(char *textptr, char newtext){
+    newtext = *textptr;
+    return newtext;
+}

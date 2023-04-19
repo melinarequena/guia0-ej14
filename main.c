fd53e9a654f32f5f21e0fbdb974a93a4f44bc03b
Melina <mrequena@fue.undef.edu.ar>
2023-04-19 19:04:02 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,3 @@
+/*Defina la función copyString que recibe un puntero, que apuntará a una cadena de
+caracteres. Al ejecutarse la función, la misma deberá retornar una copia, en una
+nueva porción de memoria */
